ecc665b25a69051acf73811ad72571eaa5476f1f
ablease <alexblease@gmail.com>
2015-02-28 17:03:16 +0000

    About Oodls feature test passsing

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
 
 class Oodls < Sinatra::Base
+
+  set :root, File.dirname(__FILE__)
+  set :views, Proc.new { File.join(root, "/views") }
+
   get '/' do
-    'Hello Oodls!'
+    erb :index
+  end
+
+  get '/about' do
+    erb :about
   end
 
   # start the server if ruby file executed directly
