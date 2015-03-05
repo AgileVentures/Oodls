18e8195ade5aaca8809e5d743c8fecd4c35c8dbf
ablease <alexblease@gmail.com>
2015-03-05 16:21:08 +0000

    Clean up app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -9,18 +9,17 @@ require_relative 'controllers/about_oodls.rb'
 require_relative 'controllers/user.rb'
 require_relative 'controllers/sign_in.rb'
 require_relative 'data_mapper_setup'
+require_relative 'helpers/helper.rb'
 
 class Oodls < Sinatra::Base
 
 	enable :sessions
-	register Sinatra::Flash
 	set :session_secret, 'super secret'
-  require_relative 'helpers/helper.rb'
+	register Sinatra::Flash
 
   set :root, File.dirname(__FILE__)
   set :views, Proc.new { File.join(root, "/views") }
 
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
 
