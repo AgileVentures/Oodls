7cc4c5cbfb2f0833c471a7668b092dc45cb631cd
Claudia <claudia.donovan89@gmail.com>
2015-03-15 15:51:23 +0000

    tidied listings controllers

--- lib/controllers/listings/new_listing_controller.rb
+++ lib/controllers/listings/new_listing_controller.rb
@@ -1,3 +1,4 @@
+#
 class Oodls < Sinatra::Base
 
   get '/listings/new' do
@@ -6,12 +7,17 @@ class Oodls < Sinatra::Base
 
   post '/listings' do
     @user = current_user
-    listing = Listing.create(:donation_centre_address => params[:address],
-  						               :donation_centre_postcode => params[:postcode],
-  							             :weekday_opening_hours => params[:weekday_hours],
-  							             :weekend_opening_hours => params[:weekend_hours],
-                             :user_id => @user.id)
-    redirect '/charity/home'
+    listing = Listing.create(donation_centre_address: params[:address],
+                             donation_centre_postcode: params[:postcode],
+                             weekday_opening_hours: params[:weekday_hours],
+                             weekend_opening_hours: params[:weekend_hours],
+                             user_id: @user.id)
+    if listing.save
+      redirect '/charity/home'
+    else
+      flash[:errors] = listing.errors.full_messages
+      redirect '/listings/new'
+    end
   end
 
 end
--- lib/controllers/listings/show_all_listings_controller.rb
+++ lib/controllers/listings/show_all_listings_controller.rb
@@ -1,3 +1,4 @@
+#
 class Oodls < Sinatra::Base
 
   get '/listings/all' do
