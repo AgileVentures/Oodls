49df879dda4397e663f9b8785512e8d1f83f4247
Claudia <claudia.donovan89@gmail.com>
2015-03-15 16:03:38 +0000

    ran rubocop on the repo and it has seriously damaged my calm

--- lib/app.rb
+++ lib/app.rb
@@ -4,23 +4,25 @@ require 'sinatra/partial'
 require 'data_mapper'
 require 'rack-flash'
 
-Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/controllers/**/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/controllers/**/*.rb'].each { |file| require file }
 
 require_relative './data_mapper_setup.rb'
 require_relative 'helper_methods/helper.rb'
 
+#
 class Oodls < Sinatra::Base
 
-	enable :sessions
-	set :session_secret, 'super secret'
-  
+  enable :sessions
+  set :session_secret, 'super secret'
+
   use Rack::Flash
   use Rack::MethodOverride
 
   set :root, File.dirname(__FILE__)
-  set :views, Proc.new { File.join(root, "/views") }
+  set :views, proc { File.join(root, '/views') }
   set :public_folder, 'public'
 
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
+
 end
