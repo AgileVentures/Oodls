5d92246c41ef9f56e3a768575b97024dcd8392b2
Claudia <claudia.donovan89@gmail.com>
2015-03-15 14:21:21 +0000

    tidied models

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,36 +1,35 @@
+#
 require 'bcrypt'
 
 class User
+  
+  include DataMapper::Resource
 
-	include DataMapper::Resource
-
-	property :id, 							Serial 
-	property :organisation, 		String, :required => true, :unique => true
-	property :contact_name,			String
-	property :email, 						String, :required => true, :unique => true
-	property :password_digest,	Text#, :length => 8..20
+  property :id, 							Serial
+  property :organisation, 		String, required: true, unique: true
+  property :contact_name,			String
+  property :email, 						String, required: true, unique: true
+  property :password_digest,	Text#, length: 8..20
   property :description,      String
   property :website,          String
 
-  has n, :listings, :constraint => :destroy
+  has n, :listings, constraint: :destroy
 
-	attr_reader 	:password
-	attr_accessor :password_confirmation
+  attr_reader   :password
+  attr_accessor :password_confirmation
 
-	validates_confirmation_of :password
-  # will soon have val_pres_of pass but not yet as it will break stuff until i have a method
+  validates_confirmation_of :password
+  # will soon have val_pres_of pass but not yet as it will break stuff
 
-	def password=(password)
-		@password = password
-		self.password_digest = BCrypt::Password.create(password)
-	end
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   def self.authenticate(email, password)
-    user = first(:email => email)
+    user = first(email: email)
     if user && BCrypt::Password.new(user.password_digest) == password
       user
-    else
-      nil
     end
   end
 
